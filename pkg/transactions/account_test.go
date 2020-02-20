8852213c34e081d88a96f3858956454761a00cd5
asukhodko <asukhodko@gmail.com>
2020-02-20 15:34:35 +0300

    тест для Deposit

--- pkg/transactions/account_test.go
+++ pkg/transactions/account_test.go
@@ -4,5 +4,10 @@ import "testing"
 
 func TestAccount_Deposit(t *testing.T) {
 	am := NewAccountManager().(*account)
-	am = am
+	_ = am.Deposit(10)
+	_ = am.Deposit(3.34)
+	var expected float32 = 13.34
+	if am.balance != expected {
+		t.Errorf("Expected %f, got %f", expected, am.balance)
+	}
 }
